b76ec4ebae97d8dd5c523a1da0a1d9d0571c59ee
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2021-08-06 14:13:28 +0100

    correcting on abover ground ewoody pft carbon and applying it to none-woody, DPM and soil carbon

--- calculateForestCarbon.r
+++ calculateForestCarbon.r
@@ -8,11 +8,15 @@ obsYrs = c(2010, 2017, 2018)
 modYrs = 2000:2019
 
 modFiles_opt = c('mod_withHuman_vegCarbon_cv.nc', 'mod_withHuman_DPM_cs.nc')
-modFile_correct = list(c(modFiles_opt, 'mod_withHuman_deadCarbon_cs_gb.nc'),
-                       c('mod_noHuman_vegCarbon_cv.nc', 'mod_noHuman_DPM_cs.nc',
-                         'mod_noHuman_deadCarbon_cs_gb.nc'))
-tbase = 500
 
+files = paste0(c('ForestCarbon_c_veg_1-2-3-4-5', 'NoneForestCarbon_c_veg_7-8-9-10-11-12-13', 
+                 'deadCarbon_cs_gb_1', 'DPM_cs_1'), '.nc')
+
+modFile_correct = list(paste0('mod_withHuman_', files),
+                       paste0('mod_noHuman_', files))
+
+modFiles_opt = modFile_correct[[1]][[1]]
+tbase = 500
 
 yrC = sapply(obsYrs, function(i) which (i == modYrs))
 yr = 1:length(modYrs)
@@ -20,15 +24,15 @@ yr = 1:length(modYrs)
 brickDIR <- function(file) brick(paste0(dir, '/', file))
 
 obs = brickDIR(obsFile)
-mods = lapply(modFiles_opt, brickDIR)
-mask = which(!is.na(sum(obs + mods[[1]][[1]]))[])
+mods = brickDIR(modFiles_opt)
+mask = which(!is.na(sum(obs + mods[[1]]))[])
 
-correct = correct0 =  matrix(0, ncol = tbase, nrow = nlayers(mods[[1]]))
-CorrectForGridCell <- function(i, id, modVeg, modDPM, returnRes = TRUE) {
+correct = correct0 =  matrix(NaN, ncol = tbase, nrow = nlayers(mods))
+CorrectForGridCell <- function(i, id, modVeg, returnRes = TRUE) {
     idf = tbase*ceiling(id/tbase)
     idi = id - idf + tbase
     
-    tfile = paste0("temp/correct_ABGcarbon_withDMP", idf, ".csv")
+    tfile = paste0("temp/correct_ABGcarbon_ForestOnly", idf, ".csv")
     
     if (file.exists(tfile)) {
         if (idf == id || id == length(mask)) {  
@@ -39,7 +43,7 @@ CorrectForGridCell <- function(i, id, modVeg, modDPM, returnRes = TRUE) {
         }
     }
     
-    M = modVeg[i][1,] + modDPM[i][1,]; O = obs[i][1,]    
+    M = modVeg[i][1,]; O = obs[i][1,]    
     O[O<0] = 0; M[M<0] = 0
     if (all(O==O[1]) && all(M[yrC] == O[1])) C = rep(0, length(yr))
     else { 
@@ -76,23 +80,29 @@ CorrectForGridCell <- function(i, id, modVeg, modDPM, returnRes = TRUE) {
     return(invisible())
 }
 
-index = 1:length(mask)#572000#
-out = mapply(CorrectForGridCell, mask[index], index,
-             MoreArgs = list(mods[[1]], mods[[2]],  returnRes = FALSE))
-out = mapply(CorrectForGridCell, mask[index], index, MoreArgs = list(mods[[1]]))
-out = out[!sapply(out, is.null)]
+index = 1:length(mask)#272000#
+#out = mapply(CorrectForGridCell, mask[index], index,
+#             MoreArgs = list(mods,  returnRes = FALSE))
+#out = mapply(CorrectForGridCell, mask[index], index, MoreArgs = list(mods))
+#
+#out = out[!sapply(out, is.null)]
+#out[[length(out)]] =  out[[length(out)]][,!is.na(out[[length(out)]][1,])]
 corrV= do.call(cbind, out)
 
-corrected = mods[[1]][[1]]
+corrected = mods[[1]]
 corrected[!is.na(corrected)] = 0
 #corrected = log(mods[[1]][[yrC[1]]]) - log(obs[[1]])
 #corrected[corrected < -20] = -20
 
 rasterizeCorrection <- function(i) {
-    tfile = paste0("temp/calForestCorrecetedVar-", i, ".nc")
+    
+    tfile = paste0("temp/calForestCorrecetedVar-liveVeg", i, ".nc")
+    print(tfile)
     if (file.exists(tfile)) return(raster(tfile))
+    #browser()
     corrected[mask[index]] = as.numeric(corrV[i,])
-    corrected[corrected>200] = 200 
+    #browser()
+    #corrected[corrected>200] = 200 
     corrected = writeRaster(corrected, file = tfile)
     return(corrected)
 }
@@ -100,7 +110,7 @@ rasterizeCorrection <- function(i) {
 corrected = layer.apply(1:nrow(corrV), rasterizeCorrection)
 
 corrTotCarbon <- function(i, id = 1:length(mods), mods, nme) {
-    tfile = paste0(c("temp/JULES_tot_carbon_corrected-", nme, '-', i, '-', id, ".nc"),
+    tfile = paste0(c("temp/JULES_tot_carbon_corrected-liveVeg", nme, '-', i, '-', id, ".nc"),
                    collapse = '-')
     if (file.exists(tfile)) return(raster(tfile))
     print(i)
@@ -117,16 +127,24 @@ corrTotCarbon <- function(i, id = 1:length(mods), mods, nme) {
     return(totCarbon)
 }
 
-forMods <- function(mods, nm) {
+forMods <- function(mods, nm, fnames) {
     outs = lapply(1:3,
                  function(i) layer.apply(1:nlayers(corrected), corrTotCarbon, i, mods, nm))
     out3 = layer.apply(1:nlayers(corrected), corrTotCarbon, 1:3, mods, nm)
     nms = paste(modYrs, 6, 15,  sep = '-')
-    out3 = setZ(out3, as.Date(nms), 'Date')    
-    writeRaster.gitInfo(out3, paste0("outputs/ForestCarbon-", nm, '.nc'), overwrite = TRUE)
+    
+    
+    writeRaster.gitInfo.Dates <- function(r, ...) {
+        r = setZ(r, as.Date(nms), 'Date')  
+        writeRaster.gitInfo(r, ..., overwrite = TRUE)
+    }
+    fnames = paste0('outputs/cci_corrected-', sapply(fnames, filename.noPath))
+    mapply(writeRaster.gitInfo.Dates, outs, fnames)
+
+    writeRaster.gitInfo.Dates(out3, paste0("outputs/ForestCarbon-", nm, '.nc'))
     return(out3)
 }
 
 modss = lapply(modFile_correct, lapply, brickDIR)
-outs = mapply(forMods, modss, c("withHumans", "noHumans"))
+outs = mapply(forMods, modss, c("withHumans", "noHumans"), modFile_correct)
 
